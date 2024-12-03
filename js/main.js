1342fb321e46c73b6c8958742e93f89f263cf91c
Ruziev Asilbek <roziyev106@gmail.com>
2024-12-03 21:48:52 +0500

    Add main js styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-.
+
+
+const elLightModeButton = document.querySelector('.site-header-light');
+const elDarkModeButton = document.querySelector('.site-header-dark');
+
+elLightModeButton.addEventListener('click', function () {
+  document.body.classList.remove('dark-mode');
+})
+
+elDarkModeButton.addEventListener('click', function () {
+  document.body.classList.add('dark-mode');
+})
